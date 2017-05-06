584fc9f6da6c7afc0692e619429d8905765f86d9
James Cartledge <jcartledge@gmail.com>
2017-05-06 10:40:47 +1000

    Added buy blurb and GA snippet.

--- pages/buy.js
+++ pages/buy.js
@@ -17,12 +17,20 @@ function FormField({label, children}) {
 
 export default (props) => (
   <Page {...props}>
+    <h1>Buy Newtown Voices</h1>
+
+    <p>If you like the sound of these narrative poems on life in “daggy old 70s Newtown”,You can order Newtown Voices from the publisher, <a href="http://www.ginninderrapress.com.au/">Gininderra Press</a>.</p>
+    <p>Or if you can’t wait, ask for it at Inner West bookshops <a href="http://www.betterread.com.au/">Better Read Than Dead</a> (Newtown), <a href="http://www.gleebooks.com.au/home">Gleebooks</a> (Glebe) and <a href="http://berkelouw.com.au/stores/leichhardt">Berkelouw Books</a> (Leichhardt).</p>
+
+    <p>The ebook of Newtown Voices will shortly be available online through Amazon and Book Depository.</p>
+
     <ul>
       <li><a href="http://www.ginninderrapress.com.au/">Gininderra Press</a></li>
-      <li><a href="http://www.betterread.com.au/">Better Read than Dead</a></li>
+      <li><a href="http://www.betterread.com.au/">Better Read Than Dead</a></li>
       <li><a href="http://www.gleebooks.com.au/home">Gleebooks</a></li>
+      <li><a href="http://berkelouw.com.au/stores/leichhardt">Berkelouw Books</a></li>
       <li>Amazon (Coming soon)</li>
-      <li>BookDepository (Coming soon)</li>
+      <li>Book Depository (Coming soon)</li>
     </ul>
   </Page>
 );
