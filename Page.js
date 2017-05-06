13ca650b7bcfe5c395f638a178bb33f27bdadc0b
James Cartledge <jcartledge@gmail.com>
2017-05-06 11:11:34 +1000

    Added GTM to manage UA tag.

--- Page.js
+++ Page.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import {Grid, PageHeader, Navbar, Nav, NavItem} from 'react-bootstrap';
+import GoogleTagManager from './GoogleTagManager.js';
 
 const Page = (props) => (
   <div>
@@ -12,7 +13,7 @@ const Page = (props) => (
       <style>{"p,div{font-family:Cardo}form,label,input,button{font-family:sans-serif}footer{margin:3em 0 0;padding:1em;text-align:center;background-color:#f8f5f0}p a,p a:hover, li a,li a:hover{color:darkgreen}"}</style>
       <title>Newtown Voices | Sue Cartledge</title>
     </Head>
-    <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-98654391-1', 'auto');ga('send', 'pageview');</script>
+    <GoogleTagManager gtmId='GTM-N34RSG'/>
     <Grid>
       <PageHeader>Sue Cartledge <small>Poet and author</small></PageHeader>
       <Navbar>
